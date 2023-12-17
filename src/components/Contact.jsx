520273329d9c034b941159653e897dd0858fe8f6
Jay Khakim <mgmediajay@gmail.com>
2023-12-17 19:57:43 +0900

    Emailjs is done!

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,8 +17,43 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false)
 
-  const handleChange = (e)=>{}
-  const handleSubmit = (e)=>{}
+  const handleChange = (e)=>{
+    const {name, value} = e.target
+
+    setForm({...form, [name]: value})
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    setLoading(true)
+
+    emailjs.send(
+      'service_lmx9hnj', 
+      'template_kmtq6ve',
+      {
+        from_name: form.name,
+        to_name: 'Javokhirbek',
+        from_email: form.email,
+        to_email: 'mgmediajay@gmail.com',
+        message: form.message
+      },
+      'p7Py55BxSfCmrvgOd'
+      )
+      .then(()=>{
+        setLoading(false)
+        alert("Thank you! I will get back to you as soon as possible")
+
+        setForm({
+          name: "",
+          email: "",
+          message: ""
+        }, (error)=>{
+          setLoading(false)
+
+          console.log(error)
+          alert('Something went wrong')
+        })
+      })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
